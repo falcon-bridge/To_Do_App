dbbd6681bc5db735eca5bd9d98164ddf4f41074d
Aayush <falcon.bridge2000@gmail.com>
2021-11-26 04:31:36 +0530

    added feature to not submit the form if it contains emtpy todo

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -6,21 +6,35 @@ import "./CourseInput.css";
 const CourseInput = (props) => {
   const [enteredValue, setEnteredValue] = useState("");
 
+  const [isValid, setIsValid] = useState(true);
+
   const goalInputChangeHandler = (event) => {
+    if (event.target.value.trim().length > 0) {
+      setIsValid(true);
+    }
     setEnteredValue(event.target.value);
   };
 
   const formSubmitHandler = (event) => {
     event.preventDefault();
+    if (enteredValue.trim().length === 0) {
+      setIsValid(false);
+      return;
+    }
     props.onAddGoal(enteredValue);
     setEnteredValue("");
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
+      <div className={`form-control ${!isValid ? "invalid" : ""}`}>
+        {/* <label style={{ color: !isValid ? "red" : "black" }}>Course Goal</label> */}
         <label>Course Goal</label>
         <input
+          // style={{
+          //   borderColor: !isValid ? "red" : "#ccc",
+          //   background: !isValid ? "salmon" : "transparent",
+          // }}
           type="text"
           value={enteredValue}
           onChange={goalInputChangeHandler}
